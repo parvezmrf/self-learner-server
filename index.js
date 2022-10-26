5f8f9e59ea76a1ba1f358044cdb68ef2ac725ced
unknown <parvezmrf@gmail.com>
2022-10-26 17:38:30 +0600

    first server upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+const courseCollection = require('./data/data.json')
+
+app.get('/courses', (req, res) => {
+
+    res.send(courseCollection)
+})
+
+app.get('/course/:id', (req, res) => {
+
+    const ids = req.params.id;
+    const signleCourse = courseCollection?.find((p) => p.id == ids);
+    if (!signleCourse) {
+        res.send('No data found')
+    }
+    res.send(signleCourse)
+})
+
+app.listen(port, () => {
+    console.log('running', port)
+})
