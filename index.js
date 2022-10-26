b2003e6e63e20d0bf965d9c4acab73092f6078d2
unknown <parvezmrf@gmail.com>
2022-10-26 17:41:53 +0600

    find added

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ app.get('/courses', (req, res) => {
     res.send(courseCollection)
 })
 
+app.get('/course/:id', (req, res) => {
+
+    const ids = req.params.id;
+    const signleCourse = courseCollection?.find((p) => p.id == ids);
+    if (!signleCourse) {
+        res.send('No data found')
+    }
+    res.send(signleCourse)
+})
 
 app.listen(port, () => {
     console.log('running', port)
